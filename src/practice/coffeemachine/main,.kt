130b0703f86382ae33d5a102247ecbb9b5480034
Lavrov529st <lavrov986@gmail.com>
2023-03-02 22:18:16 +0200

    Stage 3

--- src/practice/coffeemachine/main,.kt
+++ src/practice/coffeemachine/main,.kt
@@ -2,19 +2,30 @@ package practice.coffeemachine
 
 class `main,` {
     fun `main,`() {
-        // Кількість стаканчиків кави, яку користувач хоче приготувати
-        println("Write how many cups of coffee you will need:")
-        val cupsOfCoffee = readLine()!!.toInt()
+        print("Write how many ml of water the coffee machine has: ")
+        val water = readLine()!!.toInt()
+        print("Write how many ml of milk the coffee machine has: ")
+        val milk = readLine()!!.toInt()
+        print("Write how many grams of coffee beans the coffee machine has: ")
+        val coffeeBeans = readLine()!!.toInt()
+        print("Write how many cups of coffee you will need: ")
+        val cups = readLine()!!.toInt()
 
-        // Обчислення кількості інгредієнтів для приготування зазначеної кількості стаканчиків кави
-        val water = cupsOfCoffee * 200
-        val milk = cupsOfCoffee * 50
-        val coffeeBeans = cupsOfCoffee * 15
+        val waterPerCup = 200 // мл
+        val milkPerCup = 50 // мл
+        val beansPerCup = 15 // грам
 
-        // Виведення результатів
-        println("For $cupsOfCoffee cups of coffee you will need:")
-        println("$water ml of water")
-        println("$milk ml of milk")
-        println("$coffeeBeans g of coffee beans")
+        val maxCupsByWater = water / waterPerCup
+        val maxCupsByMilk = milk / milkPerCup
+        val maxCupsByBeans = coffeeBeans / beansPerCup
+
+        val maxCups = minOf(maxCupsByWater, maxCupsByMilk, maxCupsByBeans)
+
+        when {
+            maxCups == cups -> println("Yes, I can make that amount of coffee")
+            maxCups > cups -> println("Yes, I can make that amount of coffee (and even ${maxCups - cups} more than that)")
+            else -> println("No, I can make only $maxCups cups of coffee")
+        }
     }
+
 }
