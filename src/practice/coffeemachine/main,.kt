763c3fd29428fb2e751157f8825c0e641020136e
Lavrov529st <lavrov986@gmail.com>
2023-03-02 22:30:07 +0200

    Stage 4

--- src/practice/coffeemachine/main,.kt
+++ src/practice/coffeemachine/main,.kt
@@ -1,30 +1,88 @@
 package practice.coffeemachine
 
 class `main,` {
+    class CoffeeMachine(
+        var water: Int = 400,
+        var milk: Int = 540,
+        var beans: Int = 120,
+        var cups: Int = 9,
+        var money: Int = 550
+    ) {
+        fun remaining() {
+            println("The coffee machine has:")
+            println("$water of water")
+            println("$milk of milk")
+            println("$beans of coffee beans")
+            println("$cups of disposable cups")
+            println("$money of money")
+        }
+
+        fun fill() {
+            println("Write how many ml of water do you want to add:")
+            water += readLine()!!.toInt()
+            println("Write how many ml of milk do you want to add:")
+            milk += readLine()!!.toInt()
+            println("Write how many grams of coffee beans do you want to add:")
+            beans += readLine()!!.toInt()
+            println("Write how many disposable cups of coffee do you want to add:")
+            cups += readLine()!!.toInt()
+        }
+
+        fun take() {
+            println("I gave you $money")
+            money = 0
+        }
+
+        fun buy() {
+            println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu")
+            val choice = readLine()!!
+
+            when (choice) {
+                "1" -> makeCoffee(250, 0, 16, 4)
+                "2" -> makeCoffee(350, 75, 20, 7)
+                "3" -> makeCoffee(200, 100, 12, 6)
+                "back" -> return
+                else -> println("Invalid choice!")
+            }
+        }
+
+        private fun makeCoffee(waterNeeded: Int, milkNeeded: Int, beansNeeded: Int, cost: Int) {
+            if (water >= waterNeeded && milk >= milkNeeded && beans >= beansNeeded && cups >= 1) {
+                println("I have enough resources, making you a coffee!")
+                water -= waterNeeded
+                milk -= milkNeeded
+                beans -= beansNeeded
+                cups--
+                money += cost
+            } else {
+                if (water < waterNeeded) {
+                    println("Sorry, not enough water!")
+                } else if (milk < milkNeeded) {
+                    println("Sorry, not enough milk!")
+                } else if (beans < beansNeeded) {
+                    println("Sorry, not enough coffee beans!")
+                } else {
+                    println("Sorry, not enough cups!")
+                }
+            }
+        }
+    }
+
     fun `main,`() {
-        print("Write how many ml of water the coffee machine has: ")
-        val water = readLine()!!.toInt()
-        print("Write how many ml of milk the coffee machine has: ")
-        val milk = readLine()!!.toInt()
-        print("Write how many grams of coffee beans the coffee machine has: ")
-        val coffeeBeans = readLine()!!.toInt()
-        print("Write how many cups of coffee you will need: ")
-        val cups = readLine()!!.toInt()
+        val machine = CoffeeMachine()
 
-        val waterPerCup = 200 // мл
-        val milkPerCup = 50 // мл
-        val beansPerCup = 15 // грам
+        while (true) {
+            println("Write action (buy, fill, take, remaining, exit):")
+            val action = readLine()!!
 
-        val maxCupsByWater = water / waterPerCup
-        val maxCupsByMilk = milk / milkPerCup
-        val maxCupsByBeans = coffeeBeans / beansPerCup
-
-        val maxCups = minOf(maxCupsByWater, maxCupsByMilk, maxCupsByBeans)
-
-        when {
-            maxCups == cups -> println("Yes, I can make that amount of coffee")
-            maxCups > cups -> println("Yes, I can make that amount of coffee (and even ${maxCups - cups} more than that)")
-            else -> println("No, I can make only $maxCups cups of coffee")
+            when (action) {
+                "buy" -> machine.buy()
+                "fill" -> machine.fill()
+                "take" -> machine.take()
+                "remaining" -> machine.remaining()
+                "exit" -> return
+                else -> println("Invalid action!")
+            }
         }
     }
 
